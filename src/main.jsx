d1ee2adcd0569c3c51c0c4ae7a829a94cb45776a
Ashutosh sharan srivastava <ashutoshsharansrivastava@gmail.com>
2026-08-23 23:18:19 +0530

    so many new changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
+
+// Ensure /api is included at the end
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
